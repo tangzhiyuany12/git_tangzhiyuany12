96974803756cd5e67cea08f17bd5919194c2f076
tangzhiyuany12 <tangzhiyuany12@163.com>
2020-02-23 09:56:37 +0800

    22112

--- flask.py
+++ flask.py
@@ -8,8 +8,8 @@ def hello_world():
     return 'Hello World!'
 
 @app.route('/test')
-def hello_world():
-    return 'test'
+def hello_world1():
+    return 'test1'
 
 if __name__ == '__main__':
     app.run("0.0.0.0",port=5007,debug=True)
